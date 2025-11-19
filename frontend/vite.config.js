ae5a9da5174c62c1a0efb65370b8ffb114e7f815
excelwizpro <jsanghe123@gmail.com>
2025-11-19 11:48:09 +0000

    Frontend update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,5 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
 
 export default defineConfig({
-  root: ".",
-
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-
-    rollupOptions: {
-      input: {
-        taskpane: resolve(__dirname, "taskpane.html")
-      },
-      output: {
-        entryFileNames: "taskpane.js",
-        chunkFileNames: "chunks/[name]-[hash].js",
-        assetFileNames: "[name].[ext]"
-      }
-    }
-  }
+  base: "/excelwizpro-frontend/", // IMPORTANT for GitHub Pages
 });
